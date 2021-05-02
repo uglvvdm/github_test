8e3a5cc373a49afefed0bd764c1f5639cabe5335
Vadim Uglov <uglvvdm@gmail.com>
2021-05-02 16:11:52 +0300

    main branch different changes to see difference vs dev branch

--- first_file.py
+++ first_file.py
@@ -1,1 +1,2 @@
 # this is to test how this whole online version control works
+#branching out newer changes to see how versions differ
--- third_file.py
+++ third_file.py
@@ -1,1 +1,2 @@
 # fml this is sooo boring
+# that is soooo much fun oh boy
